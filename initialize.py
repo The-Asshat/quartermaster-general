a2d136e4e745a96c88e57526e42b3393624d6433
The_Asshat <asshat@theasshat.net>
2022-03-16 23:07:22 -0500

    Initial framework created and logging enabled

---
+++ initialize.py
@@ -0,0 +1,21 @@
+import discord, sys, logging
+
+logger = logging.getLogger("discord")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="QuartermasterGeneral.log", encoding="utf-8", mode="w")
+handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
+logger.addHandler(handler)
+
+bot = discord.Bot
+
+@bot.slash_command()
+async def hello(ctx, name: str = None):
+    name = name or ctx.author.name
+    await ctx.respond(f"Hello {name}!")
+
+@bot.user_command(name="Say Hello")
+async def hi(ctx, user):
+    await ctx.respond(f"{ctx.author.mention} says hello to {user.name}!")
+
+
+bot.run()
